f8515a4a73b097f49b74dde07e8ac08902ba34ba
Shiva Persad <1510970@rgu.ac.uk>
2016-03-07 10:07:19 +0000

    Addition of Week 6 Lab

---
+++ week6/task3.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head> </head>
+
+<body>
+<p>
+    <?php
+        $wantedgood = "mugs";
+
+        switch ($wantedgood) {
+            case "specs":
+                echo "You have to be 16 to buy specs.";
+                break;
+            case "mugs":
+                echo "You have to be 18 to buy mugs.";
+                break;
+            case "sausage rolls":
+                echo "You have to be 21 to buy sausage rolls";
+                break;
+            default:
+                echo "Invalid good";
+        }
+    ?>
+</p>
+</body>
+</html>
