da6cc545f37c8b86b027ecde32d8013a4e781fa3
Shiva Persad <1510970@rgu.ac.uk>
2016-03-07 10:39:39 +0000

    Addition of Week 6 Lab

---
+++ week6/task5.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head> </head>
+
+<body>
+<p>
+    <?php
+        $provisionedGoods = array("specs","mugs","sausage rolls");
+        $endings = array("st","nd","rd","th");
+
+        for ($i=1;$i<=30;$i++)
+        {
+            if ($i == 1)
+                echo "On the 1st of the month no products are available";
+            elseif ($i == 2)
+                echo "On the 2nd of the month specs are available";
+            elseif ($i == 3)
+                echo "On the 3rd of the months mugs are available";
+            elseif (($i % 2 == 0) && ($i % 3 == 0))
+                echo "On the " . $i . $endings[3] . " of the month specs and mugs are available";
+            elseif (($i % 2 == 0) && ($i % 4 == 0))
+                echo "On the " . $i . $endings[3] . " of the month specs and sausage rolls are available";
+            elseif (($i % 3 == 0) && ($i % 4 == 0))
+                echo "On the " . $i . $endings[3] . " of the month mugs and sausage rolls are available";
+            elseif ($i % 2 == 0)
+                echo "On the " . $i . $endings[3] . " of the month specs are available";
+            elseif ($i % 3 == 0)
+                echo "On the " . $i . $endings[3] . " of the month mugs are available";
+        }
+    ?>
+</p>
+</body>
+</html>
