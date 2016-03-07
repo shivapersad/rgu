14784c3299dfa17f7612a6b0892c1e3bea58f135
shivapersad <shivapersad@gmail.com>
2016-03-07 17:35:11 +0000

    Testing Task 5 from Lab

--- week6/task5.php
+++ week6/task5.php
@@ -31,6 +31,35 @@
             elseif ($i % 3 == 0)
                 echo "On the " . $i . $endings[3] . " of the month mugs are available <br>";
         }
+
+        echo "<br><br>";
+
+    for ($i = 1; $i < 30; $i++)
+    {
+        $y = 0; //I'm using this as a marker to tell that at least one thing is being made
+        echo "<p>On day " . $i . " the following products are available: ";
+
+        if ($i % 2 != 0)
+        {
+            echo "Sausage Rolls ";
+            $y = 1;
+        }
+        if ($i % 3 != 0)
+        {
+            echo "Mugs ";
+            $y = 1;
+        }
+        if ($i % 4 != 0)
+        {
+            echo "Specs";
+            $y = 1;
+        }
+        if ($y != 1) //if one thing isn't being made on this day then it displays that nothing is available that day.
+        {
+            echo "NONE";
+        }
+        echo "</p>";
+    }
     ?>
 </p>
 </body>
