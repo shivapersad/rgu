41a922f8f230ecc61f1b35e64ecce109d161b8d3
Shiva Persad <1510970@rgu.ac.uk>
2016-03-07 09:59:11 +0000

    Addition of Week 6 Lab

--- week6/task2.php
+++ week6/task2.php
@@ -8,11 +8,11 @@
         $myage = 21;
         $myname = "Shiva";
 
-        if (($myage > 16) && ($myage <18))
+        if (($myage >= 16) && ($myage <18))
             echo $myname . " is able to buy specs";
-        elseif (($myage > 18) && ($myage < 21))
+        elseif (($myage >= 18) && ($myage < 21))
             echo $myname . " is able to buy specs and mugs";
-        elseif ($myage > 21)
+        elseif ($myage >= 21)
             echo $myname . " is able to buy specs, mugs and sausage rolls";
         else
             echo $myname . " is UNABLE to buy specs, mugs and sausage rolls";
