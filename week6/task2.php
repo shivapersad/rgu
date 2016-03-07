de5fadefd7601529611362214a95b37d2ab50af1
Shiva Persad <1510970@rgu.ac.uk>
2016-03-07 09:55:38 +0000

    Addition of Week 6 Lab

---
+++ week6/task2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head> </head>
+
+<body>
+<p>
+    <?php
+        $myage = 16;
+        $myname = "Shiva";
+
+        if (($myage > 16) && ($myage <18))
+            echo $myname . " is able to buy specs";
+        elseif (($myage > 18) && ($myage < 21))
+            echo $myname . " is able to buy specs and mugs";
+        elseif ($myage > 21)
+            echo $myname . " is able to buy specs, mugs and sausage rolls";
+        else
+            echo $myname . " is UNABLE to buy specs, mugs and sausage rolls";
+    ?>
+</p>
+</body>
+</html>
