53f5ed41260aa072a5bed2ffa22380866a74c0bb
Shiva Persad <1510970@rgu.ac.uk>
2016-03-07 10:44:08 +0000

    Addition of Week 6 Lab

--- week6/task5.php
+++ week6/task5.php
@@ -16,12 +16,12 @@
                 echo "On the 2nd of the month specs are available <br>";
             elseif ($i == 3)
                 echo "On the 3rd of the months mugs are available <br>";
+            elseif (($i % 2 == 0) && ($i % 3 == 0) && ($i % 4 == 0) )
+                echo "On the " . $i . $endings[3] . " of the month specs, mugs and sausage rolls are available <br>";
             elseif (($i % 2 == 0) && ($i % 3 == 0))
                 echo "On the " . $i . $endings[3] . " of the month specs and mugs are available <br>";
             elseif (($i % 2 == 0) && ($i % 4 == 0))
                 echo "On the " . $i . $endings[3] . " of the month specs and sausage rolls are available <br>";
-            elseif (($i % 2 == 0) && ($i % 3 == 0) && ($i % 4 == 0) )
-                echo "On the " . $i . $endings[3] . " of the month specs, mugs and sausage rolls are available <br>";
             elseif (($i % 3 == 0) && ($i % 4 == 0))
                 echo "On the " . $i . $endings[3] . " of the month mugs and sausage rolls are available <br>";
             elseif ($i % 2 == 0)
