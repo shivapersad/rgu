0a13398e489d7d459d5b18d4c4aafd8bd7730283
Shiva Persad <1510970@rgu.ac.uk>
2016-03-07 10:41:08 +0000

    Addition of Week 6 Lab

--- week6/task5.php
+++ week6/task5.php
@@ -11,21 +11,21 @@
         for ($i=1;$i<=30;$i++)
         {
             if ($i == 1)
-                echo "On the 1st of the month no products are available";
+                echo "On the 1st of the month no products are available <br>";
             elseif ($i == 2)
-                echo "On the 2nd of the month specs are available";
+                echo "On the 2nd of the month specs are available <br>";
             elseif ($i == 3)
-                echo "On the 3rd of the months mugs are available";
+                echo "On the 3rd of the months mugs are available <br>";
             elseif (($i % 2 == 0) && ($i % 3 == 0))
-                echo "On the " . $i . $endings[3] . " of the month specs and mugs are available";
+                echo "On the " . $i . $endings[3] . " of the month specs and mugs are available <br>";
             elseif (($i % 2 == 0) && ($i % 4 == 0))
-                echo "On the " . $i . $endings[3] . " of the month specs and sausage rolls are available";
+                echo "On the " . $i . $endings[3] . " of the month specs and sausage rolls are available <br>";
             elseif (($i % 3 == 0) && ($i % 4 == 0))
-                echo "On the " . $i . $endings[3] . " of the month mugs and sausage rolls are available";
+                echo "On the " . $i . $endings[3] . " of the month mugs and sausage rolls are available <br>";
             elseif ($i % 2 == 0)
-                echo "On the " . $i . $endings[3] . " of the month specs are available";
+                echo "On the " . $i . $endings[3] . " of the month specs are available <br>";
             elseif ($i % 3 == 0)
-                echo "On the " . $i . $endings[3] . " of the month mugs are available";
+                echo "On the " . $i . $endings[3] . " of the month mugs are available <br>";
         }
     ?>
 </p>
