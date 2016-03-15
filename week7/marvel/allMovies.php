317899969649cfdf9a51501d1e16fd2b591f4afd
Shiva Persad <1510970@rgu.ac.uk>
2016-03-15 10:00:28 +0000

    Week 7 continued - Marvel Movies website

---
+++ week7/marvel/allMovies.php
@@ -0,0 +1,49 @@
+<?php
+
+    include_once("connection.php");
+
+    $sql = "SELECT * FROM marvelmovies";
+    $result = mysqli_query($db,$sql);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Marvel Movies</title>
+        <link type="text/css" rel="stylesheet" href="style.css"/>
+    </head>
+
+    <body>
+        <h1>Marvel Movies</h1>
+
+        <h2>Display All Movies</h2>
+
+        <table>
+            <tr>
+                <th>Marvel Movie ID</th>
+                <th>Release Year</th>
+                <th>Title</th>
+                <th>Production Studio</th>
+                <th>Notes</th>
+            </tr>
+            <tr>
+                <?php
+                    while ($row = $result -> fetch_array())
+                    {
+                        echo "<td>" . $row['marvelMovieID'] . "</td>";
+                        echo "<td>" . $row['yearReleased'] . "</td>";
+                        echo "<td>" . $row['title'] . "</td>";
+                        echo "<td>" . $row['productionStudio'] . "</td>";
+                        echo "<td>" . $row['notes'] . "</td>";
+                    }
+                ?>
+            </tr>
+        </table>
+    </body>
+</html>
+
+<?php
+    mysql_close($db);
+?>
---
+++ week7/marvel/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Marvel Movies</title>
+        <link type="text/css" rel="stylesheet" href="style.css"/>
+    </head>
+
+    <body>
+        <h1>Marvel Movies Database</h1>
+
+        Actions:
+            <ul>
+                <li><a href="allMovies.php">Display All Movies</a></li>
+                <li><a href="createdByMarvel.php">Display movies created by Marvel Studios</a></li>
+                <li><a href="createdAfter2010.php">Display All Movies created after 2010</a></li>
+                <li><a href="allX-MenFilms.php">Display all X-Men Films</a></li>
+            </ul>
+
+        <footer>
+            Copyright 2016
+        </footer>
+    </body>
+</html>
