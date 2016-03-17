4a1d471c125579d7077fc34221616be671a75c50
Shiva Persad <shivapersad@gmail.com>
2016-03-17 11:29:31 +0000

    Added functions.php for common functions Checking auto_increment variable for users table

--- cristars/createNewMember.php
+++ cristars/createNewMember.php
@@ -11,8 +11,26 @@
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    echo "<pre>";
-    print_r($_POST);
-    echo "</pre>";
+    // insert username and password into users table
+    $sql = "INSERT INTO users VALUES (NULL,'$username','$password')";
+    $result = mysqli_query($db,$sql);
+
+    // query the database for the AUTO INCREMENT value used for the last insert query
+    $sql = "SELECT `AUTO_INCREMENT`
+            FROM  INFORMATION_SCHEMA.TABLES
+            WHERE TABLE_SCHEMA = 'db_cristars'
+            AND   TABLE_NAME   = 'users'";
+
+    $result = mysqli_query($db,$sql);
+
+    $auto_increment = mysqli_fetch_assoc($result);
+
+    Print_R($auto_increment);
+
+    // insert data into members table
+
+
+    // echo $_POST array for easier debugging of data
+    Print_R($_POST);
 
 ?>
---
+++ cristars/functions.php
@@ -0,0 +1,10 @@
+<?php
+
+    function Print_R($var)
+    {
+        echo "<pre>";
+        print_r($var);
+        echo "</pre>";
+    }
+
+?>
