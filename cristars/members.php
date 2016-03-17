dc782e0a84f9816ee591e8fdac80ce1300decc43
shivapersad <shivapersad@gmail.com>
2016-03-17 23:16:30 +0000

    Work commenced on members.php

---
+++ cristars/members.php
@@ -0,0 +1,62 @@
+<?php
+
+    include_once("connection.php");
+
+    $id = $_GET["id"];
+
+    echo $id;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Cristars Badminton Club</title>
+    <link type="text/css" rel="stylesheet" href="assets/css/style.css" />
+    <link type="text/css" rel="stylesheet" href="assets/css/unsemantic-grid-responsive-tablet.css" />
+    <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+
+<header>
+    <!-- <h1>Cristars Badminton Club</h1> -->
+    <img src="assets/images/logo.jpg" alt="Cristars Badminton Club"/>
+
+    <div id="menu">
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="about.html">About the Club</a></li>
+            <li><a href="login.php">Members Sign In</a></li>
+            <li><a href="players.html">Our Players</a></li>
+        </ul>
+    </div>
+
+</header>
+
+<main>
+
+    <section>
+        <h2>Cristars Badminton Club</h2>
+
+        <h3>Player's Information</h3>
+
+
+    </section>
+
+</main>
+
+<br><br>
+
+<footer>&copy;2016 Cristars Badminton Club</footer>
+
+</body>
+
+</html>
+
+<?php
+
+    mysqli_close($db);
+
+?>
