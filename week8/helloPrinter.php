eff4677bf2a6761c95aff75618d39256a97c4d9f
Shiva Persad <1510970@rgu.ac.uk>
2016-03-21 09:36:10 +0000

    Week 8

---
+++ week8/helloPrinter.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Week 8 - Hello World 2.0</title>
+</head>
+
+    <body>
+
+        <header>
+
+        </header>
+
+        <main>
+
+
+        </main>
+
+        <br><br>
+
+        <footer></footer>
+
+    </body>
+
+</html>
---
+++ week8/helloSelector.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Week 8 - Hello World 2.0</title>
+</head>
+
+    <body>
+
+        <header>
+            <h1>Hello World 2.0</h1>
+        </header>
+
+        <main>
+            <p>
+                Where would you like to say hello?
+
+                <a href="helloPrinter.php?planet=earth">Earth</a>
+                <a href="helloPrinter.php?planet=mars">Mars</a>
+                <a href="helloPrinter.php?planet=uranus">Uranus</a>
+            </p>
+
+        </main>
+
+        <br><br>
+
+        <footer>Hello World 2.0</footer>
+
+    </body>
+
+</html>
