f559df1adc25ee78aa5c0cf43a755cb31f97bcf8
Shiva Persad <1510970@rgu.ac.uk>
2016-04-11 09:49:26 +0100

    Week 10

---
+++ week10/checklogin.php
@@ -0,0 +1,11 @@
+<?php
+
+    $username = $_POST["name"];
+    $password = $_POST["password"];
+
+    if ($username == "username" && $password="password")
+    {
+        setcookie('access_level_cookie','standarduser');
+    }
+
+    header('Location: loggedin.php');
