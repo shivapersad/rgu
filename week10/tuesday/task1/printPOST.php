968f195e35f2f0fc79e322c2a81664203d326902
Shiva Persad <1510970@rgu.ac.uk>
2016-04-12 10:27:38 +0100

    Week 10 - Task 2

--- week10/tuesday/task1/printPOST.php
+++ week10/tuesday/task1/printPOST.php
@@ -1,3 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Using Forms to Send Information</title>
+    </head>
 
-    print_r($_POST);
+    <body>
+
+        <article>
+            <h1><? echo $_POST["title"]; ?></h1>
+            <p><? echo $_POST["content"]; ?></p>
+            <strong> <? echo $_POST["category"]; ?></strong>
+        </article>
+
+    </body>
+</html>
+
