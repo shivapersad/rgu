ab0c79ebca413598e47ee680e30d96b83717f198
Shiva Persad <1510970@rgu.ac.uk>
2016-04-12 09:34:57 +0100

    Week 10 - Self Referencing Forms

---
+++ week10/tuesday/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Self Referencing Forms</title>
+</head>
+<body>
+    <?
+
+        if ($_SERVER['REQUEST_METHOD'] === 'GET')
+        {
+    ?>
+    <form action="<? {$_SERVER['PHP_SELF'];} ?>" method="post">
+        <label>Forename</label>
+        <input type="text" name="forename">
+        <label>Surname</label>
+        <input type="text" name="surname">
+        <p><input type="submit" value="Submit"></p>
+    </form>
+    <?
+        }
+        elseif ($_SERVER['REQUEST_METHOD'] === 'POST')
+        {
+            $forename = $_POST["forename"];
+            $surname = $_POST["surname"];
+            print("<h1>Hello {$forename} {$surname}</h1>");
+        }
+
+    ?>
+</body>
+</html>
